65bfad6c8df0ca6f41a13dd4a1e271ff94f3d432
Gabriel Aires <54085028+GabrielAires07@users.noreply.github.com>
2020-01-30 23:33:53 -0300

    Desafio 03-1 concluído.

--- Desafio01-3/Desafio03_1.js
+++ Desafio01-3/Desafio03_1.js
@@ -4,6 +4,6 @@ const usuarios = [
     { nome: 'Tuane', tecnologias: ['HTML', 'Node.js'] }
   ]
 
-  for (let i = 0; i < usuarios.length; i++) {
-      console.log (`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+  for (value of usuarios) {
+      console.log (`${value.nome} trabalha com ${value.tecnologias}`)
   }
